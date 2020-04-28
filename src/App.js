936cd110218b1f011c5548b293b4e5e6a5026fea
Zach Gates <zgates@cloud49.com>
2020-04-28 12:40:58 -0500

    added onmove

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import mapboxgl from "mapbox-gl";
+import "./App.css";
 
 function App() {
+  const [mapMeta, setMapMeta] = useState({ lng: 5, lat: 34, zoom: 2 });
+  let mapContainer = null;
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: mapContainer,
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [mapMeta.lng, mapMeta.lat],
+      zoom: mapMeta.zoom,
+    });
+    map.on("move", () => {
+      setMapMeta({
+        lng: map.getCenter().lng.toFixed(4),
+        lat: map.getCenter().lat.toFixed(4),
+        zoom: map.getZoom().toFixed(2),
+      });
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="mapContainer" ref={(el) => (mapContainer = el)} />
     </div>
   );
 }
